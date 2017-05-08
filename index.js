0aeabe8c751dedc6babe61f7068146bb5a96e878
Matus Kislan <matuskislan@gmail.com>
2017-05-08 15:18:45 +0200

    added eslint, babel, rework structure

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-const util = require("util");
-const parser = require('pg-query-parser');
-
-const Linter = require('./linter');
-
-
-
+import Linter from './lib/linter';
 
 const config = {
-    rules : {
-        "relation_check": {
-            status: 'WARNING'
-        },
-        "targetList_check": {
-            status: 'WARNING',
-        },
-    }
+  rules: {
+    relationCheck: {
+      status: 'WARNING',
+    },
+    targetListCheck: {
+      status: 'WARNING',
+    },
+  },
 };
 
 const linter = new Linter(config);
@@ -39,65 +33,66 @@ GROUP by c.output1
 HAVING (SELECT * FROM test_table4 t5) > 50`;
 
 // const SQLquery = 'SELECT c.output1, c.output1 as output2, c.output1 output3 FROM test_schema.test_table';
-// const SQLquery = 'SELECT c.output1, c.output1 as output2, c.output1 output3 FROM test_schema.test_table c LIMIT 1 GROUP by c.output1';
+// const SQLquery = `SELECT c.output1, c.output1 as output2, c.output1 output3
+// FROM test_schema.test_table c LIMIT 1 GROUP by c.output1`;
 
 // const SQLquery = 'DELETE from test_schema where id = 2000';
 
-// const SQLquery = 'SELECT c.output1, c.output1 as output2, c.output1 output3 FROM test_schema.test_table c JOIN test_table2 c2 on c.id = c2.id';
+// const SQLquery = `SELECT c.output1, c.output1 as output2, c.output1 output3
+// FROM test_schema.test_table c JOIN test_table2 c2 on c.id = c2.id`;
 
 // const SQLquery = 'SELECT c.output1, c.output1 as output2, c.output1 output3 FROM test_schema.test_table';
 
 const scheme = {
-    databaseName: 'dumpData',
-    schemaName: 'test_schema',
-    tables: [
+  databaseName: 'dumpData',
+  schemaName: 'test_schema',
+  tables: [
+    {
+      tableName: 'test_table',
+      columns: [
         {
-            tableName: 'test_table',
-            columns: [
-                {
-                    columnName: 'output1',
-                    isNullable: true,
-                    dataType: 'integer'
-                },
-                {
-                    columnName: 'output11',
-                    isNullable: true,
-                    dataType: 'integer'
-                },
-            ]
-        },  
-        {
-            tableName: 'test_table2',
-            columns: [
-                {
-                    columnName: 'output22',
-                    isNullable: true,
-                    dataType: 'integer'
-                },    
-            ]
-        }, 
-        {
-            tableName: 'test_table3',
-            columns: [
-                {
-                    columnName: 'output33',
-                    isNullable: true,
-                    dataType: 'integer'
-                },    
-            ]
+          columnName: 'output1',
+          isNullable: true,
+          dataType: 'integer',
         },
         {
-            tableName: 'test_table4',
-            columns: [
-                {
-                    columnName: 'output33',
-                    isNullable: true,
-                    dataType: 'integer'
-                },    
-            ]
-        }, 
-        
-    ]
+          columnName: 'output11',
+          isNullable: true,
+          dataType: 'integer',
+        },
+      ],
+    },
+    {
+      tableName: 'test_table2',
+      columns: [
+        {
+          columnName: 'output22',
+          isNullable: true,
+          dataType: 'integer',
+        },
+      ],
+    },
+    {
+      tableName: 'test_table3',
+      columns: [
+        {
+          columnName: 'output33',
+          isNullable: true,
+          dataType: 'integer',
+        },
+      ],
+    },
+    {
+      tableName: 'test_table4',
+      columns: [
+        {
+          columnName: 'output33',
+          isNullable: true,
+          dataType: 'integer',
+        },
+      ],
+    },
+  ],
 };
 
 // const scheme = {
@@ -111,21 +106,17 @@ const scheme = {
 //                     columnName: 'id',
 //                     isNullable: true,
 //                     dataType: 'integer'
-//                 },    
+//                 },
 //                 {
 //                     columnName: 'customer',
 //                     isNullable: true,
 //                     dataType: 'character varying'
 //                 },
 //             ]
-//         },   
+//         },
 //     ]
 // };
 
-
-const rr = parser.parse(SQLquery);
-
-const AST = rr.query;
 // console.log(rr.error);
 
 const result = linter.lint(SQLquery, scheme);
--- rules/index.js
+++
@@ -1,7 +0,0 @@
-const relation_check = require("./relation_check");
-const targetList_check = require("./targetList_check");
-
-module.exports = {
-    relation_check,
-    targetList_check,
-}
